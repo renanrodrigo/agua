128bea8fb20a957ece49e3f013258d75a6756bf3
Renan Rodrigo <rebarbos@redhat.com>
2020-11-28 09:23:45 +0100

    chore: remove redux, saga, axios, etc
    Signed-off-by: Renan Rodrigo <rebarbos@redhat.com>

--- src/services/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://worldtimeapi.org/api/',
-});
-
-export default api;
--- src/store/ducks/rootReducer.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import localtime from './localtime';
-
-export default combineReducers({
-  localtime,
-});
