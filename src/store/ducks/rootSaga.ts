d27403189eaf36e678d83c535cde3b8a93902f16
Renan Rodrigo <rebarbos@redhat.com>
2020-11-28 09:23:45 +0100

    chore: remove redux, saga, axios, etc
    Signed-off-by: Renan Rodrigo <rebarbos@redhat.com>

--- src/store/ducks/rootSaga.ts
+++
@@ -1,8 +0,0 @@
-import { all, takeLatest } from 'redux-saga/effects';
-
-import load from './localtime/sagas';
-import { LocaltimeTypes } from './localtime/types';
-
-export default function* rootSaga() {
-  return yield all([takeLatest(LocaltimeTypes.LOAD_REQUEST, load)]);
-}
